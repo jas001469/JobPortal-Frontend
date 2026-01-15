fe86830802a85bc85bb56bdb9ceb67d38ba1e44e
Jas Singh Ajmani <jas001469@gmail.com>
2026-01-15 13:55:13 +0530

    commit

--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -1,9 +1,9 @@
-// app/jobs/page.tsx
-
 import { Suspense } from "react";
 import JobsClient from "./JobsClient";
 
+export const runtime = "edge";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function JobsPage() {
   return (
