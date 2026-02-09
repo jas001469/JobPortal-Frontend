cb08246bb318e219b6c490e3e1e52dcb0a1831de
Jas Singh Ajmani <jas001469@gmail.com>
2026-02-09 22:29:37 +0530

    commit

--- frontend/app/jobs/all-jobs/page.tsx
+++ frontend/app/jobs/all-jobs/page.tsx
@@ -570,7 +570,7 @@ export default function JobsPage() {
                 className="text-xs bg-white px-3 py-1.5 rounded-full border border-gray-300 hover:border-red-600 hover:text-red-700 transition-colors"
               >
                 {city}
-              </button>k
+              </button>
             ))}
           </div> */}
 
